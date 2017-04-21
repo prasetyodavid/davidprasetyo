58e0fe0e62e8057da8c17b28553bdcb25525ccc7
David Prasetyo <davidpras1994@gmail.com>
2017-04-21 17:43:40 +0700

    Update related-post.js

--- js/related-post.js
+++ js/related-post.js
@@ -177,7 +177,7 @@ function printRelatedLabels_thumbs() {
     document.write('<div style="clear: both;"/>');
 
     while (i < relatedTitles.length && i < 30 && i < maxresults) {
-        document.write('<a title="'+ relatedTitles[i] +'" style="text-decoration:none;margin:0 4px 10px 0;float:left;');
+        document.write('<a title="'+ relatedTitles[relatedTitlesNum] +'" style="text-decoration:none;margin:0 4px 10px 0;float:left;');
         if (i != 0) document.write('"');
         else document.write('"');
 
