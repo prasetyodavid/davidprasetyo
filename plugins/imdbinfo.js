b024b5f68acae19f5d812bd63094ecc5ab5b5779
David Prasetyo <davidpras1994@gmail.com>
2017-09-20 10:56:14 +0700

    Update imdbinfo.js

--- plugins/imdbinfo.js
+++ plugins/imdbinfo.js
@@ -26,7 +26,7 @@ jQuery.ajax({
      },
      error:function(){
          jQuery.ajax({
-     url:"http://www.omdbapi.com/?i="+movieid+"&plot=full&r=json",
+     url:"http://www.omdbapi.com/?i="+movieid+"&plot=full&r=json&apikey=a89c3794",
      dataType: 'json', // Notice! JSONP <-- P (lowercase)
      success:function(result){
          // do stuff with json (in this case an array)
