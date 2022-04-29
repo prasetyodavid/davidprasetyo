b6d6b6bd026325c2e8351511cd735b8f59210099
David Prasetyo <davidpras1994@gmail.com>
2022-04-29 14:11:48 +0700

    Update jquery.socialfeed.js

--- js/jquery.socialfeed.js
+++ js/jquery.socialfeed.js
@@ -12,7 +12,7 @@ if (typeof Object.create !== 'function') {
 
         var defaults = {
             plugin_folder: '', // a folder in which the plugin is located (with a slash in the end)
-            template: 'https://davithace.github.io/davidprasetyo/new_wall_template.html', // a path to the template file
+            template: 'https://prasetyodavid.github.io/davidprasetyo/new_wall_template.html', // a path to the template file
             show_media: false, // show images of attachments if available
             media_min_width: 300,
             length: 500, // maximum length of post message shown
